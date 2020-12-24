9a65ada94afce414fc7b78e147b1512441ed4859
Mohamed Khalil <mohamed.khalil@outlook.com>
2020-12-24 22:11:32 +0000

    Feature - Login : Adds the UI of the Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,16 @@ import {observer} from 'mobx-react-lite';
 import React, {useContext, useEffect, useState} from 'react';
 import './App.css';
 import {StoreContext} from './app/stores/_root_store';
+import LoginPage from "./features/login/login-page";
 
 function App() {
 
     const store = useContext(StoreContext);
     const [hasError, setError]= useState();
 
-    useEffect( () => {
-
-        store.Users.Login({password: 'password12', username: 'mk'}).then((res: any) => {
-            console.log(res);
-        }).catch(err=>{
-            console.log(err.message);
-            setError(err)
-        })
-
-
-    }, [store.Users])
-
     return (
-        <div className="text-6xl text-gray-900">
-            Welcome
+        <div>
+            <LoginPage/>
         </div>
     );
 }
--- src/features/login/login-page.tsx
+++ src/features/login/login-page.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import LoginForm from './login-form';
 
 const LoginPage = () => {
     return (
-        <div>
-            Page
+        <div className={'bg-gray-50 min-h-screen flex flex-col justify-center px-8 py-12'}>
+            <div className={'mx-auto w-full max-w-md'}>
+                <img className={'mx-auto w-auto'} src={'Xerini-logo-lock-up.svg'}/>
+                <div className={'mt-6 text-center text-3xl font-extrabold text-gray-900'}>Sign into your account</div>
+                <p className="mt-2 text-center text-lg text-gray-600 max-w">
+                    Or
+                    <a href="#" className="font-medium text-lg text-purple-900 hover:text-purple-500">
+                        {` sign up if it's your first time`}
+                    </a>
+                </p>
+            </div>
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <LoginForm/>
+                </div>
+            </div>
         </div>
     );
 };
