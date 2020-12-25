37489c092382e1b6d02a4cd412c57728475de297
Mohamed Khalil <mohamed.khalil@outlook.com>
2020-12-25 21:41:37 +0000

    Adding placeholder icons

--- src/features/dashboard/dashboard-page.tsx
+++ src/features/dashboard/dashboard-page.tsx
@@ -8,7 +8,7 @@ const DashboardPage = () => {
             <div className={'max-w-5xl mx-auto flex justify-center'}>
 
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className={'w-64 h-64' +
-                ' text-gray-600 border-4 border-dashed rounded-lg border-gray-400'}>
+                ' text-gray-600 border-4 border-dashed rounded-full border-gray-400'}>
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
                 </svg>
--- src/features/projects/projects-page.tsx
+++ src/features/projects/projects-page.tsx
@@ -7,7 +7,7 @@ const ProjectsPage = () => {
             <div className={'max-w-5xl mx-auto flex justify-center'}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
                      className={'w-64 h-64' +
-                     ' text-gray-600 border-4 border-dashed rounded-lg border-gray-400'}>
+                     ' text-gray-600 border-4 border-dashed rounded-full border-gray-400'}>
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                           d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
                 </svg>
