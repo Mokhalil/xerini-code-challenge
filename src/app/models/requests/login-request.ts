6dbfaf951e74de76724d80f455b08c78c3a8c813
Mohamed Khalil <mohamed.khalil@outlook.com>
2020-12-25 15:19:43 +0000

    Feature - Sign-in Process : user will be automatically logged in if they logged before. And end-users will be redirected to the sign-in page if they have't or the token has expired.

--- src/app/models/requests/login-request.ts
+++ src/app/models/requests/login-request.ts
@@ -2,9 +2,10 @@ export class LoginRquest{
     username : string = '';
     password : string ='';
 
-    constructor(username:string, password: string) {
-        this.username = username;
-        this.password = password;
+    constructor(username?:string, password?: string,rememberMe?:boolean) {
+        this.username = (username)?username:'';
+        this.password = (password)?password:'';
+        this.rememberUser = (rememberMe)?rememberMe:false;
     }
 }
 
--- src/features/login/login-page.tsx
+++ src/features/login/login-page.tsx
@@ -2,6 +2,33 @@ import React from 'react';
 import LoginForm from './login-form';
 
 const LoginPage = () => {
+
+    //Context & states
+    const history = useHistory();
+    const store = useContext(StoreContext);
+    const [error, setError]=useState();
+    const [loading, setLoading]=useState<boolean>(false);
+
+    //handlers
+    const onSubmit = async (values: any) => {
+        let returned : IUser;
+
+        try {
+            setLoading(true);
+            returned = await store.Users.Login(values);
+            console.log(returned);
+            if(returned)
+                history.push('/dashboard')
+        }
+        catch (err){
+            console.log(err.message);
+            setError(err.message);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+
     return (
         <div className={'bg-gray-50 min-h-screen flex flex-col justify-center px-8 py-12'}>
             <div className={'mx-auto w-full max-w-md'}>
@@ -16,7 +43,7 @@ const LoginPage = () => {
             </div>
             <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
                 <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                    <LoginForm/>
+                    <LoginForm onSubmit={onSubmit} loading={loading} error={error}/>
                 </div>
             </div>
         </div>
