c570461ca0b51293fc4f91fb0dc08eb30d1c874d
Mohamed Khalil <mohamed.khalil@outlook.com>
2020-12-25 11:21:08 +0000

    Feature - Navigation : Factoring out navigation bar

--- src/app/layout/application-shell.tsx
+++ src/app/layout/application-shell.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
+import NavigationBar from "../common/navigation-bar";
 
 const ApplicationShell = (props:any) => {
     return (
-        <div>
-            <nav className={'max-w-7xl mx-auto px-10 bg-purple-800'}>
-                <div className={'flex flex-row items-center'}>
-                    <div>
-                        <img className={'h-12 w-64 fill-current text-gray-500'} src={'Xerini-logo-lock-up.svg'}/>
-                    </div>
-                    <div className={'ml-10'}>
-                        <div className={'space-x-4 items-baseline'}>
-                            <a href="#" className="bg-purple-700 text-white px-3 py-2 rounded-md text-sm font-medium">Dashboard</a>
-                            <a href="#" className="text-white hover:bg-purple-500 hover:bg-opacity-75 px-3 py-2 rounded-md text-sm font-medium">Project</a>
-                            <a href="#" className="text-white hover:bg-purple-500 hover:bg-opacity-75 px-3 py-2 rounded-md text-sm font-medium">Team</a>
-                            <a href="#" className="text-white hover:bg-purple-500 hover:bg-opacity-75 px-3 py-2 rounded-md text-sm font-medium">Reports</a>
-                        </div>
-                    </div>
-                </div>
-            </nav>
+        <div className={'max-w-screen'}>
+            <NavigationBar/>
             <header className={'bg-white shadow'}>
                 <div className={'max-w-7xl px-10 py-4 mx-auto'}>
                     <h1 className={'text-3xl font-bold leading-tight text-gray-900'}>
