ca425c2230c09448bdcd5edd44790977d87e8057
Mohamed Khalil <mohamed.khalil@outlook.com>
2020-12-25 11:24:01 +0000

    Feature - Application Routing : Adds routing between pages

---
+++ src/app/layout/private-route.tsx
@@ -0,0 +1,25 @@
+import { observer } from 'mobx-react-lite';
+import React, {useContext} from 'react';
+import { Route, Redirect,RouteComponentProps,RouteProps } from 'react-router-dom';
+import {StateStore, StoreContext} from "../stores/_root_store";
+
+
+interface IProps extends RouteProps {
+    component: React.ComponentType<RouteComponentProps<any>>
+}
+const PrivateRoute:React.FC<IProps> = ({component: Component, ...rest}) => {
+
+    const store = useContext(StoreContext);
+
+    return (
+        // Show the component only when the user is logged in
+        // Otherwise, redirect the user to /signin page
+        <Route {...rest} render={(props:any) => (
+            store.Users.IsLoggedIn ?
+                <Component {...props} />
+                : <Redirect to="/signin" />
+        )} />
+    );
+};
+
+export default observer(PrivateRoute);
